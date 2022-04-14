3997aab478526abc592f9a09beb7bc6bb1fbd661
игорь рева <igoryan.reva.01@mail.ru>
2022-04-14 14:45:28 +0300

    HomeWork

---
+++ WebApi/Controllers/HomeTaskController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services;
+using System.Collections.Generic;
+using System.Linq;
+using WebApi.Dto;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeTaskController : ControllerBase
+    {
+        private readonly HomeTaskService _homeTaskService;
+
+        public HomeTaskController(HomeTaskService homeTaskService)
+        {
+            _homeTaskService = homeTaskService;
+        }
+        // GET: api/HomeTask
+        [HttpGet]
+        public ActionResult<IEnumerable<HomeTaskDto>> Get()
+        {
+            return Ok(_homeTaskService.GetAllHomeTasks().Select(homeTask => HomeTaskDto.FromModel(homeTask)));
+        }
+        // Post api/HomeTask
+        [HttpPost]
+        public ActionResult Create([FromBody] HomeTaskDto value)
+        {
+            var result = _homeTaskService.CreateHomeTask(value.ToModel());
+            if (result.HasErrors)
+                return BadRequest(result.Errors);
+            return Accepted();
+        }
+
+        // GET api/HomeTask/5
+        [HttpGet("{id}")]
+        public ActionResult<HomeTaskDto> Get(int id)
+        {
+            var homeTask = _homeTaskService.GetHomeTaskById(id);
+
+            if (homeTask == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(HomeTaskDto.FromModel(homeTask));
+        }
+
+        // PUT api/HomeTask/5
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] HomeTaskDto value)
+        {
+            var updateResult = _homeTaskService.UpdateHomeTask(value.ToModel());
+            if (updateResult.HasErrors)
+            {
+                return BadRequest(updateResult.Errors);
+            }
+            return Accepted();
+        }
+
+        // DELETE api/HomeTask/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            _homeTaskService.DeleteHomeTask(id);
+            return Accepted();
+        }
+    }
+}
